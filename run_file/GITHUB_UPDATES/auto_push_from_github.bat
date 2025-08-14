f7e9489d31683aac8193c397e2d972c8a5ff8283
richer18 <richeralanano@gmail.com>
2025-08-14 16:53:33 +0800

    Auto-commit on 2025-08-14 16:53:33

---
+++ run_file/GITHUB_UPDATES/auto_push_from_github.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running Git Auto Push...
+python C:\xampp\htdocs\ETSA\run_file\GITHUB_UPDATES\auto_push_from_github.py
+pause
--- run_file/auto_push.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running Git Auto Push...
-python C:\xampp\htdocs\ETSA\run_file\git_auto_push.py
-pause
